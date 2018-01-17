97837af1e6e843521661e69d57d872bbcc97060c
Pawepek <elgharak@gmail.com>
2018-01-17 13:50:37 +0100

    Add files via upload

---
+++ IOPoprawione/IO/User.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <vector>
+using namespace std;
+class User {
+	string username;
+	string password;
+	string name;
+	string surname;
+	string email;
+	int id;
+
+	bool isLogged;
+public:
+	User(string username, string password, string name, string surname, string email);
+
+	string getUsername();
+
+	string getEmail();
+
+	string getName();
+
+	string getSurname();
+
+	bool isLoggedIn();
+
+	void registerUser(string username, string password, string name, string surname, string email, vector<User> userzy, vector <size_t> hashedpasswords);
+
+	string authenticateUser(string username, string password);
+
+	User();
+
+};
